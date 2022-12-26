37f6a41323d61253446d86748350518aafb6af71
Nikita <soznik@skbkontur.ru>
2022-12-26 22:02:17 +0500

    Upd cashier screen

--- app/src/main/java/com/plcoding/navigationdrawercompose/screens/CashierScreen.kt
+++ app/src/main/java/com/plcoding/navigationdrawercompose/screens/CashierScreen.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.plcoding.navigationdrawercompose.ui.theme.NavigationDrawerComposeTheme
 
 @Composable
@@ -24,7 +28,11 @@ fun CashierScreen(
     onSellByCacheClick: () -> Unit,
     onSellByCardClick: () -> Unit
 ) {
-    Scaffold{
+    Scaffold(
+        topBar = {
+            AppToolBar("Кассир")
+        }
+    ) {
         Column(
             Modifier
                 .padding(it)
@@ -32,24 +40,32 @@ fun CashierScreen(
                 .fillMaxSize(),
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "Название: $productName")
+            Text(text = "Название: $productName", fontSize = 18.sp)
             Spacer(Modifier.height(32.dp))
-            Text(text = "Цена: $productPrice")
+            Text(text = "Цена: $productPrice", fontSize = 18.sp)
             Spacer(Modifier.height(32.dp))
             Row {
-                Button(
+                OutlinedButton(
                     modifier = Modifier.weight(1f),
                     onClick = onSellByCacheClick
                 ) {
-                    Text(text = "Наличные")
+                    Text(text = "Наличные", fontSize = 18.sp)
                 }
                 Spacer(Modifier.width(16.dp))
-                Button(
+                OutlinedButton(
                     modifier = Modifier.weight(1f),
                     onClick = onSellByCardClick
                 ) {
-                    Text(text = "Безнал")
+                    Text(text = "Безнал", fontSize = 18.sp)
                 }
+            }
+            Spacer(Modifier.height(16.dp))
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onSellByCardClick,
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
+            ) {
+                Text(text = "Отмена", fontSize = 18.sp)
             }
         }
     }
@@ -57,7 +73,7 @@ fun CashierScreen(
 
 @Preview
 @Composable
-fun CashierScreenPreview(){
+fun CashierScreenPreview() {
     NavigationDrawerComposeTheme {
         CashierScreen(
             productName = "Торт Муравейник",
